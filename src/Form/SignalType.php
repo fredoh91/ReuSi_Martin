fef4843767591deb476efff4c842e355a9667747
fredoh91 <fredoh@gmail.com>
2023-06-27 09:48:29 +0200

    MaJ attribute

--- src/Form/SignalType.php
+++ src/Form/SignalType.php
@@ -23,38 +23,38 @@ class SignalDescType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-        ->add('id',HiddenType::class, ['required' => false])
-        ->add('dateCreation',DateType::class, ['required' => false, 'format' => 'dd-MM-yyyy', 'disabled' => true])
-        ->add('description',TextType::class, ['required' => false])
-        ->add('indication',TextType::class, ['required' => false])
-        ->add('contexte',TextType::class, ['required' => false])
-        ->add('niveauRisqueInitial',TextType::class, ['required' => false])
-        ->add('niveauRisqueFinal',HiddenType::class, ['required' => false])
-        ->add('anaRisqueComment',TextType::class, ['required' => false])
-        ->add('proposReducRisque',HiddenType::class, ['required' => false])
-        ->add('refSignal',TextType::class, ['required' => false])
-        ->add('identifiantSource',TextType::class, ['required' => false, 'label' => 'Identifiant source du signal(id cas marquant...)'])
-        ->add('sourceSignal', EntityType::class, [
-            'class' => SourceSignal::class,
-            'choice_label' => 'Libelle',
-        ])
-        ->add('poleDS', EntityType::class, [
-            'class' => PoleDS::class,
-            'choice_label' => 'Libelle',
-        ])
-        ->add('dmm', EntityType::class, [
-            'class' => DMM::class,
-            'choice_label' => 'Libelle',
-        ])
-        ->add('statutSignal', EntityType::class, [
-            'class' => StatutSignal::class,
-            'choice_label' => 'Libelle',
-        ])
-        ->add('statutEmetteur', EntityType::class, [
-            'class' => StatutEmetteur::class,
-            'choice_label' => 'Libelle',
-        ])
-        ->add('submit', SubmitType::class, ['label' => 'Enregistrer']);
+            ->add('id', HiddenType::class, ['required' => false])
+            ->add('dateCreation', DateType::class, ['required' => false, 'format' => 'dd-MM-yyyy', 'disabled' => true])
+            ->add('description', TextType::class, ['required' => false])
+            ->add('indication', TextType::class, ['required' => false])
+            ->add('contexte', TextType::class, ['required' => false])
+            ->add('niveauRisqueInitial', TextType::class, ['required' => false])
+            ->add('niveauRisqueFinal', HiddenType::class, ['required' => false])
+            ->add('anaRisqueComment', TextType::class, ['required' => false])
+            ->add('proposReducRisque', HiddenType::class, ['required' => false])
+            ->add('refSignal', TextType::class, ['required' => false])
+            ->add('identifiantSource', TextType::class, ['required' => false, 'label' => 'Identifiant source du signal(id cas marquant...)'])
+            ->add('sourceSignal', EntityType::class, [
+                'class' => SourceSignal::class,
+                'choice_label' => 'Libelle',
+            ])
+            ->add('poleDS', EntityType::class, [
+                'class' => PoleDS::class,
+                'choice_label' => 'Libelle',
+            ])
+            ->add('dmm', EntityType::class, [
+                'class' => DMM::class,
+                'choice_label' => 'Libelle',
+            ])
+            ->add('statutSignal', EntityType::class, [
+                'class' => StatutSignal::class,
+                'choice_label' => 'Libelle',
+            ])
+            ->add('statutEmetteur', EntityType::class, [
+                'class' => StatutEmetteur::class,
+                'choice_label' => 'Libelle',
+            ])
+            ->add('submit', SubmitType::class, ['label' => 'Enregistrer']);
     }
 
     public function getBlockPrefix()
